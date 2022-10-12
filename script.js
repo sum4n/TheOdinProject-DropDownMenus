ed9ae01fa5fec98a2b47458a023310982006160f
Suman Das <sum4ndas@gmail.com>
2022-10-12 07:42:40 +0530

    Toggle menu with classList.toggle.

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ let hoverBtn = document.getElementById("hover");
 let menuitem1 = document.querySelector(".menu-container1");
 let menuitem2 = document.querySelector(".menu-container2");
 
-clickBtn.addEventListener('click', () => {
-    showMenu(menuitem1)
+clickBtn.addEventListener('click', (e) => {
+    // Get element's position with offsetLeft.
+    showMenu(menuitem1, e);
 });
 
-hoverBtn.addEventListener('click', () => {
-    showMenu(menuitem2)
+hoverBtn.addEventListener('click', (e) => {
+    showMenu(menuitem2, e);
 });
 
-function showMenu(menuDiv) {
-    if (menuDiv.style.display !== "block") {
-        menuDiv.style.display = "block";
-    } else {
-        menuDiv.style.display = "none";
-    }
+function showMenu(menuDiv, parent) {
+    let position = parent.target.offsetLeft + 'px';
+
+    // if (menuDiv.style.display !== "block") {
+    //     menuDiv.style.display = "block";
+    //     menuDiv.style.left = position;
+    //     menuDiv.style.backgroundColor = window.getComputedStyle(parent.target).backgroundColor;
+    // } else {
+    //     menuDiv.style.display = "none";
+    // }
+    menuDiv.classList.toggle("menu-display");
+    menuDiv.style.left = position;
+    menuDiv.style.backgroundColor = window.getComputedStyle(parent.target).backgroundColor;
 }
